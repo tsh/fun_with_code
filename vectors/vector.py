f592183f10e623faf868a4050ab00e66696a3942
Anton Nikulin <dr.tallin@gmail.com>
2015-07-14 20:08:41 +0300

    refactor vector in separate file

--- vectors/vector.py
+++ vectors/vector.py
@@ -1,4 +1,3 @@
-import tkinter
 import math
 
 
@@ -31,65 +30,4 @@ class Vector(object):
         if magnitude != 0:
             self.div(magnitude)
 
-
-# AHTUNG! monkey patching
-def _create_circle(self, x, y, r, **kwargs):
-    return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
-tkinter.Canvas.create_circle = _create_circle
-
-
-class Ball(object):
-    def __init__(self, position, velocity):
-        self.position = position
-        self.velocity = velocity
-
-    def update(self):
-        self.position.add(self.velocity)
-
-    def update(self, canvas):
-        width = int(canvas['width'])
-        height = int(canvas['height'])
-        if self.position.x > width:
-            self.position.x = 0
-        if self.position.y > height:
-            self.position.y = 0
-
-        self.position.add(velocity)
-
-    def render(self, canvas):
-        canvas.create_circle(location.x, location.y, 7, fill='black')
-
-
-class App(object):
-    UPDATE_TIME = 10
-
-    def __init__(self):
-        self.objects = []
-
-        self.window = tkinter.Tk()
-        self.canvas = tkinter.Canvas(self.window, bg='white', height=200, width=200)
-
-    def add_object(self, obj):
-        self.objects.append(obj)
-
-    def render(self):
-        self.canvas.delete("all")
-        for o in self.objects:
-            o.update(self.canvas)
-        for o in self.objects:
-            o.render(self.canvas)
-        self.window.after(self.UPDATE_TIME, self.render)
-
-    def run(self):
-        self.canvas.pack()
-        self.render()
-        self.window.mainloop()
-
-if __name__ == '__main__':
-    app = App()
-    location = Vector(50, 50)
-    velocity = Vector(2, 2)
-    ball = Ball(location, velocity)
-    app.add_object(ball)
-    app.run()
 
